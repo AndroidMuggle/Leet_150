ad33bafeab7dc30071ebd6f065e5ac8940feb8c2
bfyu <yubufan_1993@outlook.com>
2024-01-07 22:57:06 +0800

    feat:面试经典150题-68

--- src/100/Leet_68.kt
+++ src/100/Leet_68.kt
@@ -11,15 +11,18 @@ class Leet_68 {
         }
 
         val stack = Stack<TreeNode>()
+        val hashSet = hashSetOf<TreeNode>()
         stack.push(root)
         var maxDepth = 1
-        while (stack.size != 1) {
+        while (stack.isNotEmpty()) {
             val top = stack.peek()
-            if (top.left != null) {
+            if (top.left != null && !hashSet.contains(top.left)) {
                 stack.push(top.left)
+                hashSet.add(top.left!!)
                 maxDepth = max(maxDepth, stack.size)
-            } else if (top.right != null) {
+            } else if (top.right != null && !hashSet.contains(top.right)) {
                 stack.push(top.right)
+                hashSet.add(top.right!!)
                 maxDepth = max(maxDepth, stack.size)
             } else {
                 stack.pop()
@@ -32,4 +35,20 @@ class Leet_68 {
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+}
+
+fun main() {
+    val leet68 = Leet_68()
+
+    print(
+        "${
+            leet68.maxDepth(TreeNode(3).apply {
+                left = TreeNode(9)
+                right = TreeNode(20).apply {
+                    left = TreeNode(15)
+                    right = TreeNode(7)
+                }
+            })
+        }"
+    )
 }
